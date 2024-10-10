e186959e77da92154564e473fa42db888171ba47
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-10-10 14:44:33 -0500

    Added comments

--- src/App.js
+++ src/App.js
@@ -25,9 +25,18 @@ const App = () => {
   };
 
   return (
+    //Eventually you want to move all this logic for a dropdown component into 
+    //a reusable React component. The new component comes with two slots.
+    //one slot is for the trigger which opens/closes the dropdown
+    //the other slot is for the buttons which are getting rendered in the 
+    //dropdown's menu.
     <Dropdown
+      //prop "open this new dropdown receives "open" state
       open={open}
+
+      // Slot1 - trigger open and closes the dropdown
       trigger={<button onClick={handleOpen}>Dropdown</button>}
+       //slot 2 - menu prop is for the buttons which are getting rendered in the dropdown's menu.
       menu={[
         <button onClick={handleMenuOne}>Menu 1</button>,
         <button onClick={handleMenuTwo}>Menu 2</button>,
@@ -37,11 +46,15 @@ const App = () => {
 };
 
 
-//Create a dropdown control
+//Create a dropdown control - has three props
+//open - prop to signify open or close
+//trigger - prop is for ??
+//menu - prop containing the "buttons" for menu1 and menu2
 const Dropdown = ({ open, trigger, menu }) => {
   return (
     <div className="dropdown">
-      {trigger}
+      {trigger} 
+      {/* if open = true do this. Otherwise null*/}
       {open ? (
         <ul className="menu">
           {menu.map((menuItem, index) => (
